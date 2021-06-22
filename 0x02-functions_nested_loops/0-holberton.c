0201d61865e87a51ed27ec0931127feca779d5b6
flosocabornia <flosocabornia@gmail.com>
2021-06-22 11:15:10 -0700

    Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,26 @@
+/*
+ * File: 0-holberton.c
+ * Auth: Florencia Soca
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints Holberton, followed by a new line
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+putchar('H');
+putchar('o');
+putchar('l');
+putchar('b');
+putchar('e');
+putchar('r');
+putchar('t');
+putchar('o');
+putchar('n');
+putchar('\n');
+return (0);
+}
