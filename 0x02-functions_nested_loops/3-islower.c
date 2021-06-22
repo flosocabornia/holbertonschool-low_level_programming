2d547664cd098525ee084819be4193a6b3aff8c4
flosocabornia <flosocabornia@gmail.com>
2021-06-22 13:03:57 -0700

    lowecase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
-  * _islower -
+  * _islower - return 1 if c is lowercase and returns 0 otherwise
   * @c: takes in a character
   * Return: 0 for uppercase, 1 for lowercase
   */
