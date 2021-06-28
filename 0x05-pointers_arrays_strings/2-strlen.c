5dca6859e8fe67704da77440f66cfd51cbe1cb28
flosocabornia <flosocabornia@gmail.com>
2021-06-28 12:14:43 -0700

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,22 +5,16 @@
 #include "holberton.h"
 
 /**
-* 2-starlen.c -a function that returns the length of a string
+* _starlen -a function that returns the length of a string
 * @s: puntero
 * Return: (0)
 */
 int _strlen(char *s);
 {
-char s[1000];
 int c = 0;
 
-printf("Input a string\n");
-gets(s);
-
-while (s[c] != '\0')
+while (*s[c] != '\0')
 c++;
 
-printf("Length of the string: %d\n", c);
-
-return (0);
+return (c);
 }
