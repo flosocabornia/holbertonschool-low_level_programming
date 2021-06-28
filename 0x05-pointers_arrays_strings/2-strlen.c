065da4bf2cd20b8f763d2e4a7cfd178a94455a79
flosocabornia <flosocabornia@gmail.com>
2021-06-28 12:05:08 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+/*
+ * File: 2-starlen.c
+ * Auth: Florencia Soca
+ */
+#include "holberton.h"
+
+/**
+* 2-starlen.c -a function that returns the length of a string
+* @s: puntero
+* Return: (0)
+*/
+int _strlen(char *s);
+{
+char a[20] = "Program";
+char b[20] = {'P', 'r', 'o', 'g', 'r', 'a', 'm', '\0'};
+
+printf("Length of string a = %zu \n", strlen(a));
+printf("Length of string b = %zu \n", strlen(b));
+
+}
+return (0);
+}
