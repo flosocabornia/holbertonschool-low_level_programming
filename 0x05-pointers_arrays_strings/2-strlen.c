047d2a08ff4d69942e9e0aa0afb5c1b4d94f544e
flosocabornia <flosocabornia@gmail.com>
2021-06-28 12:22:21 -0700

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,25 +3,25 @@
  * Auth: Florencia Soca
  */
 #include "holberton.h"
-
-/**
-* _starlen -a function that returns the length of a string
-* @s: puntero
-* Return: (0)
-*/
 int _strlen(char *s);
 int main() {
-  char str[20], *pt;
-  int i = 0;
-  printf("Pointer Example Program : Find or Calculate Length of String \n");
-  printf("Enter Any string [below 20 chars] : ");
-  gets(str);
-  pt = str;
-  while (*pt != '\0') {
-    i++;
-    pt++;
-  }
-  printf("Length of String : %d", i);
-
-  return 0;
+   char str[100];
+   int len;
+   
+   printf("\nEnter string : ");
+   gets(str);
+   
+   len = stringLengthUsingPointer(str);
+   printf("The length of the string %s is : %d", str, len);
+   return 0;
+}
+ 
+int stringLengthUsingPointer(char*p) /* p=&str[0] */
+{
+   int count = 0;
+   while (*p != '\0') {
+      count++;
+      p++;
+   }
+   return count;
 }
