6d093c13b96b2c061ab1d61479fc873fcc18b0d1
flosocabornia <flosocabornia@gmail.com>
2021-06-28 12:17:22 -0700

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -10,11 +10,26 @@
 * Return: (0)
 */
 int _strlen(char *s);
+
+void main() {
+char str[20];
+int length;
+clrscr();
+   
+printf("\nEnter any string : ");
+gets(str);
+   
+length = string_ln(str);
+printf("The length of the given string %s is : %d", str, length);
+getch();
+}
+ 
+int string_ln(char*p) /* p=&str[0] */
 {
-int c = 0;
-
-while (*s[c] != '\0')
-c++;
-
-return (c);
+int count = 0;
+while (*p != '\0') {
+count++;
+p++;
+}
+return count;
 }
