6863245f275077d75363aa3d58c4459edc6a6803
flosocabornia <flosocabornia@gmail.com>
2021-06-28 11:27:35 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+/*
+ * File: 1-swap.c
+ * Auth: Florencia Soca
+ */
+#include "holberton.h"
+/**
+*swap_int - swaps the values of two integers.
+*@a: value to be evaluate.
+*@b: value to be evaluate.
+*Return: not.
+*/
+void swap_int(int *a, int *b);
+{
+
+	int n = *a;
+	*a = *b;
+	*b = n;
+}
