a97c87dba369ed43871808f8e52041717831a307
flosocabornia <flosocabornia@gmail.com>
2021-06-28 09:15:58 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+/*
+ * File: reset_to_98
+ * Auth: Florencia Soca
+ */
+#include "holberton.h"
+
+/**
+* reset_to_98 -a function that takes a pointer to an int
+* @n: puntero
+* Return: (0)
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
