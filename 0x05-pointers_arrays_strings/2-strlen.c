6464d43b4aa4494ad0ec35ef7984b733699556a6
flosocabornia <flosocabornia@gmail.com>
2021-06-28 12:44:33 -0700

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -10,14 +10,12 @@
 * Return: (0)
 */
 int _strlen(char *s);
-int len1, len2;
+{
+int i;
 
-char string1[] = "Hello";
-char string2[] = {'c', 'o', 'm', 'p', 'u', 't', 'e', 'r', '\0'};
-
-len1 = strlen(string1);
-len2 = strlen(string2);
-
-printf("Length of string1 is: %d \n", len1);
-printf("Length of string2 is: %d \n", len2);
+for (i = 0; *(s + i) != ´\0´; i++)
+{
+continue;
+}
+return (i);
 }
