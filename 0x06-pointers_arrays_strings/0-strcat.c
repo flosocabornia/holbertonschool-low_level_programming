6cd9145f6402f0be0d27af4fd4afcc818a361817
flosocabornia <flosocabornia@gmail.com>
2021-07-01 12:24:24 -0700

    ej 1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* _strcat - function that links two strings
+* @dest: string 1
+* @src: string 2
+*
+* Return: a pointer to the resulting string dest
+*/
+char *_strcat(char *dest, char *src);
+{
+int a, b;
+
+a = 0, b = 0;
+
+while (dest[i])
+i++;
+while (src[j])
+dest [i++] = src [j++];
+return (dest);
+}
